6b9bf038a8aed11ccde3b45faf40e1f5e741b5ce
faiqnoov <mfaiqn@gmail.com>
2022-01-29 11:13:45 +0700

    logging users out; getting current user

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,13 +2,27 @@
   <div id="nav">
     <router-link :to="{ name: 'Dashboard' }">Dashboard</router-link> |
     <router-link :to="{ name: 'Customers' }">Customers</router-link> | 
-    <router-link :to="{ name: 'Reservations' }">Reservations</router-link>
+    <router-link :to="{ name: 'Reservations' }">Reservations</router-link> |
+    <button @click="handleClick">Logout</button>
   </div>
   <router-view/>
 </template>
 
 <script>
-export default {
+import getUser from '../composables/getUser'
 
+// firebase
+import { auth } from '../firebase/config'
+import { signOut } from 'firebase/auth'
+
+export default {
+  setup() {
+    const { user } = getUser()
+
+    const handleClick = () => {
+      signOut(auth)
+    }
+    return { handleClick, user }
+  }
 }
 </script>
